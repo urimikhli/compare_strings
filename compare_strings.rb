779909d91b3ab3793ef8104586fbd0651d6c2e55
Uri Mikhli <urimikhli@gmail.com>
2024-12-29 20:34:08 -0500

    DRY some repeat code.

--- compare_strings.rb
+++ compare_strings.rb
@@ -10,11 +10,10 @@
         while b_index < b.length
             if exact_match
                 (match, a_index, a_char) = match_character_and_increment(a, b, a_char, a_index, b_index, match)
-                return true if match.length == a.length
             elsif !exact_match
                 (match, a_index, a_char) = match_character_and_increment(a.downcase, b.downcase, a_char.downcase, a_index, b_index, match)
-                return true if match.length == a.length
             end
+            return true if match.length == a.length
             b_index += 1
         end
         false
@@ -22,18 +21,14 @@
 
     def match_character_and_increment(a, b, a_char, a_index, b_index, match)
         if a_char == b[b_index]
-            match << a_char
-            a_index += 1
-            a_char = a[a_index]
+            return match_and_increment_string(a, a_char, a_index, match)
         elsif match.length > 0
             match = []
             a_index = 0
             a_char = a[a_index]
 
             if a_char == b[b_index]
-                match << a_char
-                a_index += 1
-                a_char = a[a_index]
+                return match_and_increment_string(a, a_char, a_index, match)
             end
         end
         return match, a_index, a_char
