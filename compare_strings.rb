5da8e5321b0fb5a3d6ad1c0e69ef3a9a3ba9bb95
Uri Mikhli <urimikhli@gmail.com>
2024-12-29 20:16:29 -0500

    refactor. Simply exact_match use cases
    One method call with slightly different input values for the different values of exact_match

--- compare_strings.rb
+++ compare_strings.rb
@@ -1,41 +1,42 @@
 
 #module CompareStrings
+    require 'byebug'
+
     def is_a_within_b(a, b, exact_match=true)
         match=[]
         a_index = 0
         a_char = a[a_index]
         b_index = 0
         while b_index < b.length
-            if exact_match && a_char == b[b_index]
-                match << a_char
-                a_index += 1
-                a_char = a[a_index]
+            if exact_match
+                (match, a_index, a_char) = match_character_and_increment(a, b, a_char, a_index, b_index, match)
                 return true if match.length == a.length
-            elsif !exact_match && a_char.downcase == b[b_index].downcase
-                match << a_char
-                a_index += 1
-                a_char = a[a_index]
+            elsif !exact_match
+                (match, a_index, a_char) = match_character_and_increment(a.downcase, b.downcase, a_char.downcase, a_index, b_index, match)
                 return true if match.length == a.length
-            elsif match.length > 0
-                match = []
-                a_index = 0
-                a_char = a[a_index]
-
-                if exact_match && a_char == b[b_index]
-                    match << a_char
-                    a_index += 1
-                    a_char = a[a_index]
-                    return true if match.length == a.length
-                elsif !exact_match && a_char.downcase == b[b_index].downcase
-                    match << a_char
-                    a_index += 1
-                    a_char = a[a_index]
-                    return true if match.length == a.length
-                end
-
             end
             b_index += 1
         end
         false
     end
+
+    def match_character_and_increment(a, b, a_char, a_index, b_index, match)
+        if a_char == b[b_index]
+            match << a_char
+            a_index += 1
+            a_char = a[a_index]
+        elsif match.length > 0
+            match = []
+            a_index = 0
+            a_char = a[a_index]
+
+            if a_char == b[b_index]
+                match << a_char
+                a_index += 1
+                a_char = a[a_index]
+            end
+        end
+        return match, a_index, a_char
+    end
+
 #end
--- tests.rb
+++ tests.rb
@@ -1,84 +1,203 @@
 require './compare_strings'
 
-# Test 1
-string1 = "The"
-string2_match = "welcome to The jungle"
-exact_match = true
-if is_a_within_b(string1, string2_match, exact_match)
-    puts "passed: matching string"
-else
-    puts "failed: matching string"
+
+def test1_exact_match
+    string1 = "The"
+    string2_match = "welcome to The jungle"
+    exact_match = true
+    if is_a_within_b(string1, string2_match, exact_match)
+        puts "passed: matching string"
+    else
+        puts "failed: matching string"
+    end
 end
 
-# Test 2
-string1 = "The"
-string2_nomatch = "We've got fun and games"
-exact_match = true
-if is_a_within_b(string1, string2_nomatch, exact_match)
-    puts "failed: non matching string"
-else
-    puts "passed: non matching string"
+def test2_exact_match
+    string1 = "The"
+    string2_nomatch = "We've got fun and games"
+    exact_match = true
+    if is_a_within_b(string1, string2_nomatch, exact_match)
+        puts "failed: non matching string"
+    else
+        puts "passed: non matching string"
+    end
 end
 
-# Test 3
-string1 = "The"
-string2_match = "welcome to TThe jungle"
-exact_match = true
-if is_a_within_b(string1, string2_match, exact_match)
-    puts "passed: matching string next char edgecase"
-else
-    puts "failed: matching string next char edgecase"
+def test3_exact_match
+    string1 = "The"
+    string2_match = "welcome to TThe jungle"
+    exact_match = true
+    if is_a_within_b(string1, string2_match, exact_match)
+        puts "passed: matching string next char edgecase"
+    else
+        puts "failed: matching string next char edgecase"
+    end
 end
 
-# Test 4 
-# can work for sequences
-s1 = [1,'a',3]
-s2_matching = [1,3,'f',5,1,'a',3,'d',6,7,8,'h']
-exact_match = true
-if is_a_within_b(s1, s2_matching, exact_match)
-    puts "passed: matching sequences"
-else
-    puts "failed: matching sequences"
+def test4_exact_match
+    # can work for sequences
+    s1 = [1,'a',3]
+    s2_matching = [1,3,'f',5,1,'a',3,'d',6,7,8,'h']
+    exact_match = true
+    if is_a_within_b(s1, s2_matching, exact_match)
+        puts "passed: matching sequences"
+    else
+        puts "failed: matching sequences"
+    end
 end
 
-# Test 5 
-# can work for sequences
-s1 = [1,'a',3]
-s2_non_matching = [1,3,'f',5,1,'a',43,'d',6,7,8,'h']
-exact_match = true
-if is_a_within_b(s1, s2_non_matching, exact_match)
-    puts "failed: NON matching sequences"
-else
-    puts "passed: NON matching sequences"
+def test5_exact_match
+    # can work for sequences
+    s1 = [1,'a',3]
+    s2_non_matching = [1,3,'f',5,1,'a',43,'d',6,7,8,'h']
+    exact_match = true
+    if is_a_within_b(s1, s2_non_matching, exact_match)
+        puts "failed: NON matching sequences"
+    else
+        puts "passed: NON matching sequences"
+    end
 end
 
-# Test 6
-# can work for sequences
-s1 = [1,'a',3]
-s2_matching = [1,3,'f',5,1,1,'a',3,'d',6,7,8,'h']
-exact_match = true
-if is_a_within_b(s1, s2_matching, exact_match)
-    puts "passed: matching sequences next char edgecase"
-else
-    puts "failed: matching sequences next char edgecase"
+def test6_exact_match
+    # can work for sequences
+    s1 = [1,'a',3]
+    s2_matching = [1,3,'f',5,1,1,'a',3,'d',6,7,8,'h']
+    exact_match = true
+    if is_a_within_b(s1, s2_matching, exact_match)
+        puts "passed: matching sequences next char edgecase"
+    else
+        puts "failed: matching sequences next char edgecase"
+    end
 end
 
-# Test 7
-string1 = "wel"
-string2_match = "welcome to The jungle"
-exact_match = true
-if is_a_within_b(string1, string2_match, exact_match)
-    puts "passed: matching string"
-else
-    puts "failed: matching string"
+def test7_exact_match
+    string1 = "wel"
+    string2_match = "welcome to The jungle"
+    exact_match = true
+    if is_a_within_b(string1, string2_match, exact_match)
+        puts "passed: matching string"
+    else
+        puts "failed: matching string"
+    end
 end
 
-# Test 8
-string1 = "ngle"
-string2_match = "welcome to The jungle"
-exact_match = true
-if is_a_within_b(string1, string2_match, exact_match)
-    puts "passed: matching string"
-else
-    puts "failed: matching string"
+def test8_exact_match
+    string1 = "ngle"
+    string2_match = "welcome to The jungle"
+    exact_match = true
+    if is_a_within_b(string1, string2_match, exact_match)
+        puts "passed: matching string"
+    else
+        puts "failed: matching string"
+    end
 end
+
+def test1_not_exact_match
+    string1 = "The"
+    string2_match = "welcome to The jungle"
+    exact_match = true
+    if is_a_within_b(string1, string2_match, exact_match)
+        puts "passed: matching string"
+    else
+        puts "failed: matching string"
+    end
+end
+
+def test2_not_exact_match
+    string1 = "The"
+    string2_nomatch = "We've got fun and games"
+    exact_match = true
+    if is_a_within_b(string1, string2_nomatch, exact_match)
+        puts "failed: non matching string"
+    else
+        puts "passed: non matching string"
+    end
+end
+
+def test3_not_exact_match
+    string1 = "The"
+    string2_match = "welcome to TThe jungle"
+    exact_match = true
+    if is_a_within_b(string1, string2_match, exact_match)
+        puts "passed: matching string next char edgecase"
+    else
+        puts "failed: matching string next char edgecase"
+    end
+end
+
+def test4_not_exact_match
+    # can work for sequences
+    s1 = [1,'a',3]
+    s2_matching = [1,3,'f',5,1,'a',3,'d',6,7,8,'h']
+    exact_match = true
+    if is_a_within_b(s1, s2_matching, exact_match)
+        puts "passed: matching sequences"
+    else
+        puts "failed: matching sequences"
+    end
+end
+
+def test5_not_exact_match
+    # can work for sequences
+    s1 = [1,'a',3]
+    s2_non_matching = [1,3,'f',5,1,'a',43,'d',6,7,8,'h']
+    exact_match = true
+    if is_a_within_b(s1, s2_non_matching, exact_match)
+        puts "failed: NON matching sequences"
+    else
+        puts "passed: NON matching sequences"
+    end
+end
+
+def test6_not_exact_match
+    # can work for sequences
+    s1 = [1,'a',3]
+    s2_matching = [1,3,'f',5,1,1,'a',3,'d',6,7,8,'h']
+    exact_match = true
+    if is_a_within_b(s1, s2_matching, exact_match)
+        puts "passed: matching sequences next char edgecase"
+    else
+        puts "failed: matching sequences next char edgecase"
+    end
+end
+
+def test7_not_exact_match
+    string1 = "wel"
+    string2_match = "welcome to The jungle"
+    exact_match = true
+    if is_a_within_b(string1, string2_match, exact_match)
+        puts "passed: matching string"
+    else
+        puts "failed: matching string"
+    end
+end
+
+def test8_not_exact_match
+    string1 = "ngle"
+    string2_match = "welcome to The jungle"
+    exact_match = true
+    if is_a_within_b(string1, string2_match, exact_match)
+        puts "passed: matching string"
+    else
+        puts "failed: matching string"
+    end
+end
+
+# Run tests
+test1_exact_match
+test2_exact_match
+test3_exact_match
+test4_exact_match
+test5_exact_match
+test6_exact_match
+test7_exact_match
+test8_exact_match
+puts "###"
+test1_not_exact_match
+test2_not_exact_match
+test3_not_exact_match
+test4_not_exact_match
+test5_not_exact_match
+test6_not_exact_match
+test7_not_exact_match
+test8_not_exact_match
