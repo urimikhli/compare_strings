9228f1329dbf44606fb57eb68391bc1a9512eb2e
Uri Mikhli <urimikhli@gmail.com>
2025-01-01 17:04:00 -0500

    Dont need a_index

--- compare_strings.rb
+++ compare_strings.rb
@@ -10,12 +10,11 @@ module CompareStrings
             b = b.downcase if b.is_a?(String) && !exact_match
 
             number_matches_found = 0
-            a_index = 0
-            a_char = a[a_index]
+            a_char = a[0]
             b_index = 0
             while b_index < b.length
-                (number_matches_found, a_index, a_char) = match_character_and_increment(
-                    a, b, a_char, a_index, b_index, number_matches_found
+                (number_matches_found, a_char) = match_character_and_increment(
+                    a, b, a_char, b_index, number_matches_found
                 )
                 return true if number_matches_found == a.length
                 b_index += 1
@@ -25,32 +24,31 @@ module CompareStrings
 
         private
 
-        def match_character_and_increment(a, b, a_char, a_index, b_index, number_matches_found)
+        def match_character_and_increment(a, b, a_char, b_index, number_matches_found)
             if a_char == b[b_index]
-                return match_and_increment_string(a, a_char, a_index, number_matches_found)
+                return match_and_increment_string(a, a_char, number_matches_found)
             elsif number_matches_found > 0
-                (number_matches_found, a_index, a_char) = reset_matchs(a)
+                (number_matches_found, a_char) = reset_matchs(a)
 
                 if a_char == b[b_index]
-                    return match_and_increment_string(a, a_char, a_index, number_matches_found)
+                    return match_and_increment_string(a, a_char, number_matches_found)
                 end
             end
-            return number_matches_found, a_index, a_char
+            return number_matches_found, a_char
         end
 
-        def match_and_increment_string(a, a_char, a_index, number_matches_found)
+        def match_and_increment_string(a, a_char, number_matches_found)
             number_matches_found += 1
-            a_index += 1
-            a_char = a[a_index]
+            a_char = a[number_matches_found]
 
-            return number_matches_found, a_index, a_char
+            return number_matches_found, a_char
         end
 
         def reset_matchs(a)
             number_matches_found = 0
             a_index = 0
             a_char = a[a_index]
-            return number_matches_found, a_index, a_char
+            return number_matches_found, a_char
         end
     end
 end
